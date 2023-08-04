cffb6bdd93c17d6411891bb6425f03f1c4461e64
IURI <123806396+ithauront@users.noreply.github.com>
2023-08-04 17:17:26 +0200

    json and database
    create a json.js file to handle the buffer and starting to make a database file

--- src/server.js
+++ src/server.js
@@ -1,20 +1,12 @@
 import http from 'node:http'
 import {format} from 'date-fns'
+import { json } from './middleware/json.js'
+import { Database } from '../database.js'
 
-const tasks = []
+const database = new Database()
 const server = http.createServer(async(req, res) => {
     const {method, url} = req
-    const buffers = []
-
-    for await (const chunk of req) {
-        buffers.push(chunk)
-    } 
-    
-    try {
-        req.body = JSON.parse(Buffer.concat(buffers).toString()) 
-    }catch{
-        req.body = null
-    }   
+   await json(req, res)
     
    
     
@@ -22,21 +14,21 @@ const server = http.createServer(async(req, res) => {
     const formatedDate = format(currentDate, 'dd-MM-yyyy')
 
     if(method == 'GET' && url == '/tasks') {
-        return res
-        .setHeader('Content-type', 'aplication/json')
-        .end(JSON.stringify(tasks))
+       const tasks = database.select('tasks')
+        return res.end(JSON.stringify(tasks))
     }
 
     if(method == 'POST' && url == '/tasks') {
         const {title, description} = req.body
-        tasks.push({
+        const tasks = {
             id: 1,
             title,
             description,
             completed_at: null,
             created_at: formatedDate,
             updated_at: formatedDate,
-        })
+        }
+        database.insert('tasks', tasks)
         return res.writeHead(201).end()
     }
 
